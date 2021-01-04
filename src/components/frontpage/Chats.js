caeb4b47e83a868d0a3ee4f58a9b4bb65b8d7c42
Pepe <tullioangius@icloud.com>
2021-01-04 13:18:07 +0100

    comments v.2

--- src/components/frontpage/Chats.js
+++ src/components/frontpage/Chats.js
@@ -5,14 +5,15 @@ import "firebase/database";
 
 
 export default function Chats() {
-    const [chats, setChats] = useState([])
-    const [isChatting, setIsChatting] = useState(false)
-    const [currentChat, setCurrentChat] = useState({})
-    const [messages, setMessages] = useState([])
-    const [username, setUsername] = useState(''); //username current user
-    const textRef = useRef()
-    const currentUser = useSelector(state=>state.auth.user)
+    const [chats, setChats] = useState([])                  //chat del current user
+    const [isChatting, setIsChatting] = useState(false)     //stato per determinare se si devono visualizzare le chats
+    const [currentChat, setCurrentChat] = useState({})      //stato che rappresenta la chat aperta dal current user
+    const [messages, setMessages] = useState([])            //messaggi della currentChat
+    const [username, setUsername] = useState('');           //username current user
+    const textRef = useRef()                                //testo del messaggio che l'utente ha scritto
+    const currentUser = useSelector(state=>state.auth.user) //current user da store redux
 
+    //useEffect triggerato ogni volta che si apre/chiude una chat
     useEffect(()=>{
         retrieveUsername()
         retrieveCurrentUserChats()
@@ -21,16 +22,21 @@ export default function Chats() {
         }
     },[currentUser, isChatting])
 
+    //funzione per recuperare le chat del current user
     function retrieveCurrentUserChats(){
+        //ref ale chats
         var chatsRef = firebase.database().ref('chats')
         chatsRef.on('value', (snapshot)=>{
-            let chatsTemp = []
+            let chatsTemp = []  //array temporaneo per le chat
+            //loop attraverso le chat nel db
             for(var key in snapshot.val()){
+                //check se il current user è coinvolto nella chat come user1 o user2
+                //se l'utente è coinvolto viene pushato nell'array temporaneo un oggetto che rappresenta la chat
                 if(snapshot.val()[key].user1 === currentUser.uid ){
                     chatsTemp.push({
-                        chatKey: key,
-                        friendID: snapshot.val()[key].user2,
-                        friendUsername: snapshot.val()[key].user2username
+                        chatKey: key,                                       //id della chat
+                        friendID: snapshot.val()[key].user2,                //id dell'amico con cui si sta chattando
+                        friendUsername: snapshot.val()[key].user2username   //username dell'amico con cui si sta chattando
                     })
                 } else if(snapshot.val()[key].user2 === currentUser.uid){
                     chatsTemp.push({
@@ -40,10 +46,12 @@ export default function Chats() {
                     })
                 }
             }
+            //si pusha l'array temp nello stato chats
             setChats(chats=>chatsTemp)
         })
     }
 
+    //funzione per recuperare l'username del current user
     function retrieveUsername(){
         var userData = firebase.database().ref('users/' + currentUser.uid)
         userData.on('value', (snapshot) => {
@@ -53,8 +61,11 @@ export default function Chats() {
         })
     }
 
+    //funzione triggerata dell'apertura di una chat, prende come props la chat aperta
     function openChat(props){
+        //si setta lo stato "isChatting" su true per visualizzazione corretta degli elementi
         setIsChatting(true)
+        //si setta la currentChat con gli attributi dalla chat aperta
         setCurrentChat({
             idFriendChatting: props.friendID,
             chatID: props.chatKey,
@@ -62,45 +73,65 @@ export default function Chats() {
         })
     }
 
+    //funzione per caricare i messaggi
     function retrieveMessages(){
+        //ref alla chat aperta (salvata quindi nello stato currentChat illustrato nella funzione qua sopra ^)
         var messages = firebase.database().ref('chats/'+ currentChat.chatID+'/msgs')
         messages.on('value', (snapshot)=>{
             if(snapshot.val()){
-                let msgsTemp = []
+                let msgsTemp = []    //array temporaneo per salvare i messaggi
                 for(var key in snapshot.val()){
+                    //ogni messaggio ha un suo id (key)
+                    //si crea quindi un oggetto per ogni messaggio con il contenuto di esso e chi l'ha inviato
                     var messaggio = {
-                        content: snapshot.val()[key].msg,
-                        sender: snapshot.val()[key].sender,
-                        senderID: snapshot.val()[key].senderID
+                        content: snapshot.val()[key].msg,       //contenuto del messaggio
+                        sender: snapshot.val()[key].sender,     //username di chi ha mandato il messaggio
+                        senderID: snapshot.val()[key].senderID  //id di chi ha mandato il messaggio
                     }
+                    //si pusha il messaggio nell'array temporaneo
                     msgsTemp.push(messaggio)
                 }
+                //si pusha l'array temporaneo nello stato messages
                 setMessages(messages=>msgsTemp)
             }
         })
     }
 
+    //Funzione per mandare un messaggio on submit del campo di testo
     async function sendMessage(e){
+        //prevent default forse non necessario visto la rimozione del bottone ma funziona quindi ok così, non toccare per ora
         e.preventDefault()
+        //si salva il testo del messaggio
         var messaggio = textRef.current.value
+        //si svuota il campo di input
         textRef.current.value = ''
+        //ref alla chat aperta
         var messages = firebase.database().ref('chats/'+ currentChat.chatID+'/msgs')
+        //si pusha il nuovo messaggio nella chat nel db
         await messages.push({
             msg: messaggio,
             sender: username,
             senderID: currentUser.uid
         })
+        //update scroll per tenere la chat sempre sull'ultimo messaggio
         updateScroll()
     }
+
+    //funzione per chiudere la current chat e tornare alle chats del current user
     function goBack(){
+        //pulizia stati
         setIsChatting(false)
         setMessages([])
     }
+
+    //funzione per cancellare una chat, prende come prop la chat in questione
     function deleteChat(props){
+        //ref alla chat stessa e rimozione di essa
         var chat = firebase.database().ref('chats/'+props.chatKey)
         chat.remove()
     }
 
+    //funzione per scrollare, tiene la chat sull'ultimo messaggio all'apertura di essa o quando viene inviato un nuovo messaggio
     function updateScroll(){
         var element = document.getElementById("chat");
         element.scrollTop = element.scrollHeight;
@@ -110,6 +141,7 @@ export default function Chats() {
         <div className='col-6 chats m-2'>
             <h2  className='mx-auto mt-3'><i class="far fa-comments"></i> CHATS</h2>
             {chats && !isChatting && chats.map(chat=>{
+                //return delle chats se non si sta chattando
                 return (
                     <div className='chatContainer' >
                         <div onClick={()=>openChat(chat)} className='card alreadyChatting w-100 mt-2'>
@@ -120,16 +152,21 @@ export default function Chats() {
                     
                 )
             })}
+            {/*Return se si sta chattando, con nome dell'amico con cui si sta chattando */}
                 {isChatting && <h5 className = 'm-3'> <i onClick={goBack} class="backBtn fas fa-arrow-circle-left fa-lg"></i> {currentChat.usernameFriendChatting}</h5>}
+            {/*chat box dove verranno visualizzati i messaggi */}
             <div className='msgsBox' id='chat'>
                 {isChatting && messages.map(msg => {
+                    //map attraverso i messaggi retriviati all'apertura della chat
                     if(msg.senderID === currentUser.uid){
+                        //return se il current user ha mandato il messaggio (messaggio a destra, di colore #BB86FC)
                         return(
                             <div className='d-flex justify-content-end'>
                                 <p className='msg-sender'>{msg.content}</p>
                             </div>
                         )
                     } else {
+                        //return se il current user ha ricevuto il messaggio (messaggio a destra, di colore #3700B3)
                         return(
                             <div className='d-flex justify-content-start'>
                                 <p className='msg-receiver'>{msg.content}</p>
@@ -139,6 +176,7 @@ export default function Chats() {
                 })
                 }
             </div>
+            {/*display del form di input per inviare i messaggi se si sta chattando con qualcuno */}
                 {isChatting && <form onSubmit={sendMessage} className= 'row  mb-3 d-flex justify-content-center chatinput w-100'>
                     <input placeholder='message...' type='submit' ref={textRef} type='text' className='w-75 messageInput'/>
                 </form>}
--- src/components/frontpage/Friends.js
+++ src/components/frontpage/Friends.js
@@ -4,13 +4,14 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 export default function Friends() {
-    const [search, setSearch] = useState('');
-    const [searchResults, setSearchResults] = useState([])
-    const [username, setUsername] = useState(''); //current user id
-    const currentUser = useSelector(state=>state.auth.user)
-    const [friendList, setFriendList] = useState([])
-    const [chats, setChats] = useState([]) //amici che hanno già una chat con current
+    const [search, setSearch] = useState('');               //ricerca che viene settata onchange dell'input
+    const [searchResults, setSearchResults] = useState([])  //risultati della ricerca
+    const [username, setUsername] = useState('');           //current user username
+    const currentUser = useSelector(state=>state.auth.user) //current user dallo store redux
+    const [friendList, setFriendList] = useState([])        //lista degli amici
+    const [chats, setChats] = useState([])                  //amici che hanno già una chat con current
 
+    //useEffect triggerato quando avviene una ricerca
     useEffect(()=>{
         retrieveUsername()
         retrieveFriends()
@@ -30,72 +31,99 @@ export default function Friends() {
         })
     }
 
+    //viene chiamata questa funzione nell'useEffect quando è presente una ricerca
     function handleSearch(){
+        //ref degli utenti nel db, visto che la ricerca avviene per username
         var users = firebase.database().ref('users/');
         users.on('value', (snapshot) => {
-            let results = []
+            let results = [] //array temporaneo dei risultati
+            //loop atraverso tutti gli utenti
             for(var key in snapshot.val()){
                 if(snapshot.val().hasOwnProperty(key)){
+                    //si controlla se l'username dell'utente che si sta controllando includa la ricerca
                     if(snapshot.val()[key].username.includes(search)){
+                        //in caso si salva l'utente trovato in un oggetto temporaneo
                         var match = {
-                            uid: key,
-                            username: snapshot.val()[key].username,
-                            requests: snapshot.val()[key].requests,
-                            friends: snapshot.val()[key].friends
+                            uid: key,                               //id del risultato
+                            username: snapshot.val()[key].username, //username del risultato
+                            requests: snapshot.val()[key].requests, //richieste del risultato (per controllare se la richiesta è già stata inviata)
+                            friends: snapshot.val()[key].friends    //amici del risultato (per controllare se è già amico di current user)
                         }
+                        //l'oggetto viene pushato nell'array temporaneo
                         results.push(match)
                     }
                 }
             }
+            //l'array temporaneo viene passato come stato
             setSearchResults(searchResults => results)
         })
     }
 
+    //funzione per caricare gli utenti già amici
     function retrieveFriends(){
+        //ref agli amici del current user
         var friends = firebase.database().ref('users/'+ currentUser.uid + '/friends')
-        let frnd = []
+        let frnd = [] //array temporaneo degli amici del current user
         friends.on('value', (snapshot)=>{
+            //loop attraverso gli amici
             for(var key in snapshot.val()){
+                //oggetto temporanto che rappresenta l'amico
                 var currentFriend = {
-                    id: snapshot.val()[key].friendID,
-                    username: snapshot.val()[key].friendUsername
+                    id: snapshot.val()[key].friendID,               //id dell'amico
+                    username: snapshot.val()[key].friendUsername    //username dell'amico
                 }
+                //push dell'oggetto temporaneo nell'array temporaneo
                 frnd.push(currentFriend)
             }
+            //setto lo stato con l'array temporaneo degli amici
             setFriendList(friendList => frnd)
         })
     }
 
+    //Funzione per recuperare le chat
     function retrieveChats(){
+        //ref alle chat nel db
         var chats = firebase.database().ref('chats/')
         chats.on('value', (snapshot)=>{
-            let chatsTemp = []
+            let chatsTemp = [] //array temporaneo per le chat
+            //ogni chat ha: un id(key della chat), user1 e user2, che sarebbero gli utenti che stanno chattanto, i loro usernames e un array di messaggi
             for(var key in snapshot.val()){
+                //se l'id del current user corrisponde all'id di uno dei due utenti della chat, vuol dire che la chat gli appartiene
+                //Viene pushato nell'array temporano l'id dell'utente con il quale il current user sta chattanto
                 if(currentUser.uid === snapshot.val()[key].user1){
                     chatsTemp.push(snapshot.val()[key].user2)
                 } else if(currentUser.uid === snapshot.val()[key].user2){
                     chatsTemp.push(snapshot.val()[key].user1)
                 }
             }
+            //gli utenti con i quali il current user sta chattando vengono quindi storati nello stato chats
             setChats(chats=>chatsTemp)
         })
     }
 
+    //funzione per settare lo stato search col valore dell'input. questo triggera l'useEffect ^
     function handleChange(e){
         setSearch(e.target.value)
     }
 
+    //funzione per mandare richiesta di amicizia (add friend)
     function handleAdd(props){
+        //ref alle richieste del target
         var user = firebase.database().ref('users/'+ props.uid +'/requests/');
+        //push della richiesta nel db
         user.push({
-            userAsking: currentUser.uid, 
-            username: username
+            userAsking: currentUser.uid, //current user id
+            username: username           //current user username
         })
     }
 
+    //funzione per avviare chat con amico
     function newChat(props){
+        //ref alle chat
         var chats = firebase.database().ref('/chats/')
+        //push della nuova chat
         chats.push({
+            //ids e usernames dei due utenti
             user1: currentUser.uid, 
             user2: props.id,
             user1username: username,
@@ -112,22 +140,28 @@ export default function Friends() {
             {/*visualizza lista amici quando non si sta effettuando una ricerca */}
             {!search && <h2 className='mx-auto mt-3'> <i className="far fa-address-book fa-sm"> </i> Friends List</h2>}
             {!search && friendList && friendList.map(friend=>{
+                //si setta una variabile booleana usata per vedere se l'utente e l'amico stiano già chattando
                 var isChatting = false
                 chats.map(chat=>{
+                    //se l'id dell'amico corrisponde a uno degli id salvati nello stato chat --> i due stanno chattando --> non si deve poter avviare un'altra chat
                     if(chat === friend.id){
                         isChatting=true
                     } 
                 })
                 if(isChatting){
+                    //return se i due stanno chattando (nessuna funzione onClick)
                     return <p className='mx-auto my-3'>{friend.username} <i className="greenIcon fas fa-comments fa-lg"></i></p>
                 }else{
+                    //return se i due non stanno chattando (possibilità di avviare una nuova chat onClick)
                     return <p className='mx-auto my-3'>{friend.username} <i onClick={()=>newChat(friend)} className='far fa-comments fa-lg clickable'></i></p>
                 }
             })}
             {/*Visualizza ricerca onChange dell'input */}
             {search && searchResults.map(result => {
+                //si esclude il current user dai risultati della ricerca
                 if(result.username !== username){
                     for(var key in result.requests){
+                        //check per vedere se il current user ha già inviato la richiesta al target
                         if(result.requests[key].userAsking === currentUser.uid){
                             //return in caso si sia inviata la richiesta all'utente
                             return(
@@ -140,6 +174,7 @@ export default function Friends() {
                             )
                         }
                     }
+                    //check se l'utente sia già amico del current user
                     for(var key in result.friends){
                         var isFriend = false
                         friendList.map(friend=>{
